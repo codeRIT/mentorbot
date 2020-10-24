4e5b8f317732c4520b157f20ff3fe5442de11e73
Colby Gallup <colbygallup@gmail.com>
2020-10-23 20:19:23 -0400

    Added Topic model

--- src/main/java/entities/Topic.java
+++ src/main/java/entities/Topic.java
@@ -1,5 +1,54 @@
 package entities;
 
-public class Topic {
+import net.dv8tion.jda.api.entities.Member;
 
+import java.util.ArrayList;
+
+/**
+ * A topic for a server. Internally contains a queue of Members.
+ */
+public class Topic {
+    private final String name;
+    private final ArrayList<Member> queue = new ArrayList<>();
+
+    /**
+     * Constructs a new Topic object. This does not automatically create
+     * the topic on the Discord server.
+     * @param name The name of the topic.
+     */
+    public Topic(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Add a Member to the back of the queue.
+     * @param member The Member to add
+     */
+    public void addToQueue(Member member) {
+        queue.add(member);
+    }
+
+    /**
+     * Remove a Member from their position in the queue.
+     * @param member The Member to remove
+     */
+    public void removeFromQueue(Member member) {
+        queue.remove(member);
+    }
+
+    /**
+     * Remove and return the next Member in the queue.
+     * @return The Member at the front of the queue
+     */
+    public Member getNextFromQueue() {
+        return queue.remove(0);
+    }
+
+    /**
+     * Get the name of this Topic.
+     * @return This Topic's name
+     */
+    public String getName() {
+        return name;
+    }
 }
